6b48d0aaa125e4f8b734024b0152c4ccaccc8062
ftessmann <fernando.tessmann@gmail.com>
2024-04-25 11:30:14 -0300

    added functional tests

---
+++ test/jest-setup.ts
@@ -0,0 +1,8 @@
+import { SetupServer } from "@src/server";
+import supertest, { SuperTest, Test} from "supertest";
+
+beforeAll(async () => {
+    const server = new SetupServer();
+    await server.init();
+    global.testRequest = supertest(server.getApp()) as any;
+});
